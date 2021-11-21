764a15b5fff3beb8d6d43ffc5d0079a93c5a55ba
matteoiorio <iorio436@outlook.it>
2021-11-21 18:20:59 +0100

    bonus: lighter logic to display chats-update

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -2,6 +2,7 @@ var boolsApp = new Vue(
     {
         el : '#app',
         data : {
+            activeChat : 0, 
             lastAccess : dayjs().format('D:M:YYYY ore: M:ss'),
             // search in contacts list
             searchContact : "",
@@ -12,129 +13,148 @@ var boolsApp = new Vue(
                     name: 'Michele',
                     avatar: 'img/avatar_3.jpg',
                     visible: true,
-                    found : true,
                     messages: [
                         {
                             date : '10/01/2020 15:30:55',
                             text : 'Hai portato a spasso il cane?',
                             status : 'sent',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                         {
                             date : '10/01/2020 15:50:00',
                             text : 'gli hai dato da mangiare?',
                             status : 'sent',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                         {
                             date : '10/01/2020 16:15:22',
                             text : 'Tutto fatto',
                             status : 'received',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                     ],
                 },
                 {
                     name: 'Fabio',
                     avatar: 'img/avatar_1.jpg',
-                    visible: false,
-                    found : true,
+                    visible: true,
                     messages: [
                         {
                             date : '20/03/2020 16:30:00',
                             text : 'Ciao come stai?',
                             status : 'sent',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                         {
                             date : '20/03/2020 16:30:55',
                             text : 'Bene grazie, stasera ci vediamo?',
                             status : 'received',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                         {
                             date : '20/03/2020 16:35:00',
                             text : 'Mi piacerebbe, ma stasera sono impegnato',
                             status : 'sent',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                     ],
                 },
                 {
                     name: 'Samuele',
                     avatar: 'img/avatar_4.jpg',
-                    visible: false,
-                    found : true,
+                    visible: true,
                     messages: [
                         {
                             date : '28/03/2020 10:10:40',
                             text : 'Ciao, stasera cinema?',
                             status : 'sent',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                         {
                             date : '28/03/2020 10:20:10',
                             text : 'Ottima idea, a che ora?',
                             status : 'received',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                         {
                             date : '28/03/2020 16:15:22',
                             text : 'Spettacolo delle 20:30, ci vediamo là 10 minuti prima',
                             status : 'sent',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                     ],
                 },
                 {
                     name: 'Luisa',
                     avatar: 'img/avatar_2.jpg',
-                    visible: false,
-                    found : true,
+                    visible: true,
                     messages: [
                         {
                             date : '10/01/2020 15:30:55',
                             text : 'Hai comprato il pane?',
                             status : 'sent',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                         {
                             date : '10/01/2020 15:50:00',
                             text : 'Si',
                             status : 'received',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                         {
                             date : '10/01/2020 15:53:16',
                             text : 'Ottimo, grazie',
                             status : 'sent',
-                            openDropMenu : false
+                            openDropMenu : false,
+                            selected : false,
                         },
                     ],
                 },
             ]    
         },
         methods : {
-            openChat : function(contact) {
-                if(!contact.visible) {
-                    // delete every open chat before opening a new one
-                    this.contacts.forEach((contact) => {
-                        contact.visible = false;
-                    }),
-                    contact.visible = true;
+            openChat : function (i) {
+                if(this.activeChat !== i){
+                    this.activeChat = i;
                 }
             },
-            // send new message
-            sendMessage : function(contact) {
-                let newMesObj ={date: dayjs().format('D/M/YYYY ore: HH:mm:ss'), text : this.newMessage, status : 'sent', openDropMenu : false}
+             // send new message
+            sendMessage : function() {
+                let newMesObj =
+                { 
+                    date: dayjs().format('D/M/YYYY ore: HH:mm:ss'), 
+                    text : this.newMessage, 
+                    status : 'sent', 
+                    openDropMenu : false, 
+                    selected : false,
+                };
                 if(this.newMessage !== "" && this.newMessage !== " ") {
                     // push new message 
-                    contact.messages.push(newMesObj);
+                    this.contacts[this.activeChat].messages.push(newMesObj);
                     this.newMessage = "";
+                    let currentChat = this.contacts[this.activeChat].messages;
                     // get auto answer
                     setTimeout(function() {                   
-                        newMesObj = {date: dayjs().format('D/M/YYYY ore: HH:mm:ss'), text : 'ok', status : 'received', openDropMenu : false};
-                        contact.messages.push(newMesObj);
+                        newMesObj = 
+                        {
+                            date: dayjs().format('D/M/YYYY ore: HH:mm:ss'), 
+                            text : 'ok', 
+                            status : 'received', 
+                            openDropMenu : false, 
+                            selected : false,
+                        };
+                        currentChat.push(newMesObj);
                         this.newMessage = "";
                     }
                     ,1000);
@@ -145,28 +165,35 @@ var boolsApp = new Vue(
                 this.contacts.forEach((contact) => {
                     // exclude unrelated contacts
                     if(!contact.name.toLowerCase().includes(this.searchContact.toLowerCase())) {
-                        contact.found = false;
-                        console.log("questo NON è correlato: ",contact.name, contact.found);
+                        contact.visible = false;
+                        console.log("questo NON è correlato: ",contact.name, contact.visible);
                      // print only relates contacts 
                     }else {                        
-                        console.log("questo è correlato: ",contact.name, contact.found);
-                        contact.found = true;
+                        console.log("questo è correlato: ",contact.name, contact.visible);
+                   contact.visible = true;
                     }
                 })
             },
             // function to get dropdown menu to show
-            openDropdownMenu : function(contact, j) {
-                if(contact.messages[j].openDropMenu === false) {
-                    contact.messages[j].openDropMenu = true;
-                }else if(contact.messages[j].openDropMenu === true) {
-                    contact.messages[j].openDropMenu = false;
+            openDropdownMenu : function(message) {
+                if(message.openDropMenu === false) {
+                    message.openDropMenu = true;
+                }else if(message.openDropMenu === true) {
+                    message.openDropMenu = false;
+                }
+            },
+            // function to select message by message (prep to try and add possibility to delete multiple messages at once)
+            selectMessage : function(message) {
+                if(message.selected === false) {
+                    message.selected = true;
+                }else if(message.selected === true) {
+                    message.selected = false;
                 }
             },
             // function to eliminate message
-            deleteMsg : function(contact, j) {
-                contact.messages.splice(j, 1)
+            deleteMsg : function(j) {
+                this.contacts[this.activeChat].messages.splice(j, 1);
             }
-
         },
     }
 )
